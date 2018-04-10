7608291b4e296565c121dea4422482a78af60067
Wincen Kwok <wincen.kwok@gmail.com>
2018-04-10 17:40:53 -0400

    added tracking in header

--- wp-content/themes/Child of twentytwelve/header.php
+++ wp-content/themes/Child of twentytwelve/header.php
@@ -29,6 +29,17 @@
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 <?php wp_head(); ?>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-117348992-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-117348992-1');
+</script>
+
 </head>
 
 <body <?php body_class(); ?>>
